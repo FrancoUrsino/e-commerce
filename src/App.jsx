c657d90dc7d82f040823a8bbf1c5bb45f45f67f0
Franco Ursino <franncoursinno@gmail.com>
2024-10-22 12:37:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import ItemDetailsContainer from './pages/ItemDetailsContainer';
+import ItemListContainer from './pages/ItemListContainer';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+import { Checkout } from './pages/Checkout';
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100">
+          <Navbar />
+          <main className="py-8">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/productos' element={<ItemListContainer />} />
+              <Route path='/item/:id' element={<ItemDetailsContainer />} />
+              <Route path='/productos/:category' element={<ItemListContainer />} />
+              <Route path='/carrito' element={<Cart />} />
+              <Route path='/checkout' element={<Checkout />} />
+            </Routes>
+          </main>
+      </div>
+    </>
+  )
+}
+
+export default App
